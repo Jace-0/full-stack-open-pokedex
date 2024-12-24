901b3c02893780d7a556e05fb6203d17b1d25a1d
= <jacesam311@outlook.com>
2024-12-23 17:11:35 -0800

    added health check

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ const app = express()
 app.get('/version', (req, res) => {
   res.send('1') // change this string to ensure a new version deployed
 })
+
+app.get('/health', (req, res) => {
+  res.send('ok')
+})
 // get the port from env variable
 const PORT = process.env.PORT || 5000
 
